6fba5833cd117a8b878eb95d61f02525a9cb163b
Chris Timbol <christimbol707@gmail.com>
2023-01-16 17:52:19 -0800

    add try catch block

--- index.js
+++ index.js
@@ -25,52 +25,52 @@ app.get('/', async (req, res) => {
         let browser = await chromium.launch({ headless: true, chromiumSandbox: false });
         let page = await browser.newPage()
     }
-    catch{
+    catch {
         console.log('browser error')
     }
     // scroll page to make sure we get all products
     try {
-    await page.goto(url)
-    await page.keyboard.press('End');
-    await page.waitForTimeout(1000);
-    await page.keyboard.press('Home');
+        await page.goto(url)
+        await page.keyboard.press('End');
+        await page.waitForTimeout(1000);
+        await page.keyboard.press('Home');
 
-    // arrays of locators for each product
-    const dayDates = await page.locator("p[class='headline-1']").allTextContents()
-    const monthDates = await page.locator("p[class='headline-4']").allTextContents()
-    const productStyle = await page.locator("h3[class='headline-5']").allTextContents()
-    const productName = await page.locator("h6[class='headline-3']").allTextContents()
-    const productLink = await page.locator(" a[data-qa='product-card-link']").all()
-    const productImage = await page.locator('img[class="image-component mod-image-component u-full-width"]').all()
+        // arrays of locators for each product
+        const dayDates = await page.locator("p[class='headline-1']").allTextContents()
+        const monthDates = await page.locator("p[class='headline-4']").allTextContents()
+        const productStyle = await page.locator("h3[class='headline-5']").allTextContents()
+        const productName = await page.locator("h6[class='headline-3']").allTextContents()
+        const productLink = await page.locator(" a[data-qa='product-card-link']").all()
+        const productImage = await page.locator('img[class="image-component mod-image-component u-full-width"]').all()
 
-    // create a productCard of extracted data then push onto product arr
-    // i=index of each product
-    for (let i = 0; i < await page.locator('img[class="image-component mod-image-component u-full-width"]').count(); i++) {
-        const productCard = {} // layout data of each individual product\
-        productCard['site'] = url
-        productCard['product'] = productName[i]
-        productCard['date'] = monthDates[i] + " " + dayDates[i]
-        productCard['style'] = productStyle[i]
-        productCard['price'] = "Not Available"
-        productCard['img'] = await productImage[i].getAttribute('src')
-        productCard['link'] = "https://www.nike.com/" + await productLink[i].getAttribute('href')
+        // create a productCard of extracted data then push onto product arr
+        // i=index of each product
+        for (let i = 0; i < await page.locator('img[class="image-component mod-image-component u-full-width"]').count(); i++) {
+            const productCard = {} // layout data of each individual product\
+            productCard['site'] = url
+            productCard['product'] = productName[i]
+            productCard['date'] = monthDates[i] + " " + dayDates[i]
+            productCard['style'] = productStyle[i]
+            productCard['price'] = "Not Available"
+            productCard['img'] = await productImage[i].getAttribute('src')
+            productCard['link'] = "https://www.nike.com/" + await productLink[i].getAttribute('href')
 
-        products.push(productCard)
-    }
-
-    browser.close()
-    res.send(products)
-    /*     productResults.push(await Nike())
-        productResults.push(await FootLocker())
-        productResults.push(await Champs())
-        productResults.push(await Jdsports())
-        productResults.push(await Finishline()) 
-        res.send(productResults) 
-    */
-    }
-        catch{
-            console.log('everything else error')
+            products.push(productCard)
         }
+
+        browser.close()
+        res.send(products)
+        /*     productResults.push(await Nike())
+            productResults.push(await FootLocker())
+            productResults.push(await Champs())
+            productResults.push(await Jdsports())
+            productResults.push(await Finishline()) 
+            res.send(productResults) 
+        */
+    }
+    catch {
+        console.log('everything else error')
+    }
 })
 
 app.listen(port, () => {
