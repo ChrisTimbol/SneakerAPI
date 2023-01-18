f40c314efda4f9ca9ff6a2d06adbf9d1ff5a48d6
Chris Timbol <christimbol707@gmail.com>
2023-01-18 14:31:57 -0800

    add initial page with routing to api

--- api.js
+++ api.js
@@ -1,27 +1,21 @@
 require('dotenv').config()
 const path = require('path');
 const express = require('express')
-const { Scrape } = require('./scrape.js')
 const { Nike } = require('./productsites/nike.js')
 
 const app = express()
 const port = process.env.PORT || 3000
 const products = []
-app.get('/', async (req, res) => {
-    res.send(await Nike(products))
 
+app.get('/', async (req, res) => {
+    res.sendFile(path.join(__dirname, '/index.html'));
+}) 
+app.get('/api', async (req, res) => {
+    res.send(await Nike(products))
 })
+
 app.listen(port, () => {
     console.log(`ready on http://localhost:${port}`)
 })
 
-module.exports = app 
-
-
-/* 
-
-app.get('/', async (req, res) => {
-    res.sendFile(path.join(__dirname, '/index.html'));
-
-}) 
- */
+module.exports = app
