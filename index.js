16d8b4aacdbaa1b1ff601e5af61ffad7e8b5e1db
Chris Timbol <christimbol707@gmail.com>
2023-01-18 11:06:45 -0800

    scrape function and update func for db

--- index.js
+++ index.js
@@ -1,24 +1,50 @@
 require('dotenv').config()
 const { MongoClient, ServerApiVersion } = require('mongodb')
+const { Nike } = require('./productsites/nike.js')
+const { Finishline } = require('./productsites/finishline.js');
+const { FootLocker } = require('./productsites/footlocker.js')
+const { Champs } = require('./productsites/champs.js')
+const { Jdsports } = require('./productsites/jdsports.js')
 
 
 // pass client to other files
 // inside files set productcard'key' value to update
 // sleep
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@sneaker.1nvjvbv.mongodb.net/?retryWrites=true&w=majority`
-const client = new MongoClient(uri)
-async function run() {
+const client = new MongoClient(uri);
+const products = [];
+async function Scrape() {
     try {
-        const database = client.db('sneakers')
-        const coll = database.collection('sneakerCollection')
-        const query =  coll.updateOne({ product : { $exists: true }, {$set: productCard}, true })
-
-    } finally {
-  
-        await client.close();
+         await Nike(products) 
+         await Finishline(products) 
+       /*  await FootLocker(products) */
+  /*       await Champs(products)
+        await Jdsports(products)
+         */
+    } catch (err) {
+        console.log(err + "error scraping")
     }
 }
-run().catch(console.dir); 
+
+
+    (async () => {
+        try {
+            await Scrape();
+            
+            const database = client.db('sneakers')
+            const coll = database.collection('sneakerCollection')
+            for (let data of products) {
+                coll.updateOne({ data: data }, { $set: { data } }, { upsert: true })
+
+            }
+        } finally {
+            await client.close();
+        }
+
+
+    })().catch(console.dir)
+
+
 
 /*
 const express = require('express')
